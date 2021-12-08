01fcf8480668b269a6a608225f046b8d1eea103e
siddhant gore <siddhantgore111@gmail.com>
2021-12-08 22:25:35 +0530

    v2.0

--- Client/JS/client.js
+++ Client/JS/client.js
@@ -3,6 +3,7 @@ const socket = io('http://localhost:8000');
 const form = document.getElementById('send-container');
 const messageInp = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
+const messageContainer2 = document.querySelector(".container2");
 
 var ring=new Audio('/Client/Audio/ring.mp3');
 
@@ -17,6 +18,17 @@ const append=(message,position)=>{
     }
 }
 
+const append2=(message,position)=>{
+    const messageElement=document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('message');
+    messageElement.id=position;
+    messageContainer2.append(messageElement);
+}
+function remove(username) {
+    var myobj = document.getElementById(username);
+    myobj.remove();
+  }
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     const message=messageInp.value;
@@ -28,14 +40,26 @@ form.addEventListener('submit',(e)=>{
 const username = prompt("Enter Your Name to Join");
 socket.emit('new-user-joined', username);
 
-socket.on('user-joined',newuser =>{
+socket.on('user-joined',(newuser,users) =>{
     append(`${newuser} joined the chat`,'Right');
+    append2(`${newuser}`,newuser);
+    console.log(users);   
 })
-
+socket.on('online',(newuser,users) =>{
+  
+    console.log(users);
+    for (var key in users) {
+        if (users.hasOwnProperty(key)) {
+          append2(`${users[key]}`,users[key]);
+          console.log(users[key]); 
+        }
+      }   
+})
 socket.on('receive', data =>{
     append(`${data.name} : ${data.message}`,'Left');
 })
 
 socket.on('left', username =>{
     append(`${username} left`,'Left');
+    remove(username);
 })
